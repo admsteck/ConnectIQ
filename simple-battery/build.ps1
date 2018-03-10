91fbebeb6a8f97ce7b08bc84042ed84607954faa
Stechschulte, Adam M <admsteck@thestechschultes.com>
2018-03-10 11:12:25 -0500

    Move battery widget into its own folder

--- simple-battery/build.ps1
+++ simple-battery/build.ps1
@@ -1,20 +1,27 @@
 param(
     [switch]
-    $run,
+    $Run,
 
     [switch]
-    $deploy
+    $Deploy,
+
+    [switch]
+    $Publish
 )
 
 monkeyc -f .\monkey.jungle -o bin/battery-widget.prg -y C:\connectiq-sdk-win-2.4.3\developer_key.der -s 2.4.1 -d vivoactive3
 
-if ($run) {
+if ($Run) {
     if (-Not (Get-Process -Name simulator -ErrorAction SilentlyContinue)) {
         connectiq
     }
     monkeydo .\bin\battery-widget.prg vivoactive3
 }
 
-if ($deploy) {
+if ($Deploy) {
     Copy-Item .\bin\battery-widget.prg D:\GARMIN\APPS\
+}
+
+if ($Publish) {
+
 }
